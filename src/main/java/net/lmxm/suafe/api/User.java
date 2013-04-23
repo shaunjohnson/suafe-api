5c43ae80e7fc352a18df58a1c629bc5e46dd372a
Shaun Johnson <shaun.johnson@gmail.com>
2013-04-23 13:11:03 -0400

    Allow User to contain set of user groups
    Modify the User bean to allow it to contain a set of user groups in
    which the user is a member.

--- src/main/java/net/lmxm/suafe/api/User.java
+++ src/main/java/net/lmxm/suafe/api/User.java
@@ -1,28 +1,91 @@
 package net.lmxm.suafe.api;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * User information.
+ */
 public final class User {
+    /**
+     * Name of the user
+     */
     private String name;
 
+    /**
+     * Optional alias of the user.
+     */
     private String alias;
 
+    /**
+     * User groups in which this user is a member.
+     */
+    private final Set<UserGroup> userGroupsMemberOf = new HashSet<UserGroup>();
+
+    /**
+     * Construct a user with the provided name and optional alias.
+     *
+     * @param name Name of the user
+     * @param alias Optional alias of the user
+     */
     protected User(final String name, final String alias) {
         this.name = name;
         this.alias = alias;
     }
 
-    public String getName() {
-        return name;
-    }
-
+    /**
+     * Gets the current alias of the user.
+     *
+     * @return Alias of the user
+     */
     public String getAlias() {
         return alias;
     }
 
-    public void setAlias(final String alias) {
+    /**
+     * Gets the current name of the user.
+     *
+     * @return Name of the user
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets set of user groups in which this user is a member.
+     *
+     * @return Set of user groups
+     */
+    public Set<UserGroup> getUserGroupsMemberOf() {
+        return Collections.unmodifiableSet(userGroupsMemberOf);
+    }
+
+    /**
+     * Adds a user group to this user.
+     *
+     * @param userGroup User group to add
+     * @return true of user group is not already in the user, otherwise false
+     */
+    protected boolean addUserGroupMemberOf(final UserGroup userGroup) {
+        return userGroupsMemberOf.add(userGroup);
+    }
+
+    /**
+     * Changes alias for this user.
+     *
+     * @param alias New alias for the user
+     */
+    protected void setAlias(final String alias) {
         this.alias = alias;
     }
 
-    public void setName(final String name) {
+    /**
+     * Renames this user.
+     *
+     * @param name New name for the user
+     */
+    protected void setName(final String name) {
         this.name = name;
     }
 }
--- src/test/java/net/lmxm/suafe/api/UserTest.java
+++ src/test/java/net/lmxm/suafe/api/UserTest.java
@@ -3,27 +3,65 @@ package net.lmxm.suafe.api;
 import org.junit.Test;
 
 import static net.lmxm.suafe.api.CustomMatchers.*;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
 
 public final class UserTest {
     @Test
-    public void testUserGroup() {
-        assertThat(UserGroup.class, is(protectedConstructor()));
+    public void testUser() {
+        assertThat(User.class, is(protectedConstructor()));
 
-        assertThat(new UserGroup("userGroupName").getName(), is(equalTo("userGroupName")));
-        assertThat(new UserGroup("userGroupName").getUsers(), is(emptySet()));
-        assertThat(new UserGroup("userGroupName").getUsers(), is(immutableSet()));
+        assertThat(new User("userName", null).getName(), is(equalTo("userName")));
+        assertThat(new User("userName", null).getAlias(), is(nullValue()));
+
+        assertThat(new User("userName", "userAlias").getName(), is(equalTo("userName")));
+        assertThat(new User("userName", "userAlias").getAlias(), is(equalTo("userAlias")));
+
+        assertThat(new User("userName", null).getUserGroupsMemberOf(), is(emptySet()));
+        assertThat(new User("userName", null).getUserGroupsMemberOf(), is(immutableSet()));
+    }
+
+    @Test
+    public void testAddUserGroupMemberOf() {
+        assertThat(User.class, is(protectedMethod("addUserGroupMemberOf")));
+
+        final UserGroup userGroup = new UserGroup("userGroupName");
+        final User user = new User("userName", null);
+        assertThat(user.getUserGroupsMemberOf(), is(emptySet()));
+
+        userGroup.addUser(user);
+        assertThat(userGroup.getUsers(), is(not(emptySet())));
+        assertThat(userGroup.getUsers().iterator().next(), is(sameInstance(user)));
+    }
+
+    @Test
+    public void testGetUsers() {
+        final User user = new User("userName", null);
+        assertThat(user.getUserGroupsMemberOf(), is(immutableSet()));
+        assertThat(user.getUserGroupsMemberOf(), is(notNullValue()));
+    }
+
+    @Test
+    public void testSetAlias() {
+        assertThat(User.class, is(protectedMethod("setAlias")));
+
+        final User user = new User("userName", "userAlias");
+        assertThat(user.getAlias(), is(equalTo("userAlias")));
+
+        user.setAlias("newUserAlias");
+        assertThat(user.getAlias(), is(equalTo("newUserAlias")));
     }
 
     @Test
     public void testSetName() {
-        assertThat(UserGroup.class, is(protectedMethod("setName")));
+        assertThat(User.class, is(protectedMethod("setName")));
 
-        final UserGroup userGroup = new UserGroup("userGroupName");
-        assertThat(userGroup.getName(), is(equalTo("userGroupName")));
-        userGroup.setName("newUserGroupName");
-        assertThat(userGroup.getName(), is(equalTo("newUserGroupName")));
+        final User user = new User("userName", "userAlias");
+        assertThat(user.getName(), is(equalTo("userName")));
+
+        user.setName("newUserName");
+        assertThat(user.getName(), is(equalTo("newUserName")));
     }
 }
