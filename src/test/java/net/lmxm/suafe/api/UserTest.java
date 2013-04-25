1c218fb8d62cb803609c34ca56b9a0da2ab2b8c9
Shaun Johnson <shaun.johnson@gmail.com>
2013-04-24 21:40:58 -0400

    Added unit test for User.getUserGroupsMemberOf()

--- src/test/java/net/lmxm/suafe/api/UserTest.java
+++ src/test/java/net/lmxm/suafe/api/UserTest.java
@@ -42,6 +42,21 @@ public final class UserTest {
     }
 
     @Test
+    public void testGetUserGroupsMemberOf() {
+        // Setup
+        final User user = new User("userName", null);
+        final UserGroup userGroup = new UserGroup("userGroupName");
+        assertThat(user.getUserGroupsMemberOf(), is(emptySet()));
+
+        // Test
+        assertThat(user.addUserGroupMemberOf(userGroup), is(true));
+        assertThat(user.getUserGroupsMemberOf(), is(not(emptySet())));
+        assertThat(user.getUserGroupsMemberOf(), is(containsSameInstance(userGroup)));
+        assertThat(user.removeUserGroupMemberOf(userGroup), is(true));
+        assertThat(user.getUserGroupsMemberOf(), is(emptySet()));
+    }
+
+    @Test
     public void testGetUsers() {
         final User user = new User("userName", null);
         assertThat(user.getUserGroupsMemberOf(), is(immutableSet()));
@@ -50,7 +65,7 @@ public final class UserTest {
 
     @Test
     public void testRemoveUserGroupMemberOf() {
-        assertThat(User.class, is(protectedMethod("addUserGroupMemberOf")));
+        assertThat(User.class, is(protectedMethod("removeUserGroupMemberOf")));
 
         // Setup
         final User user = new User("userName", null);
