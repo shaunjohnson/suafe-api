7f5768e7266fb8a604509bb092feda5205a37b70
Shaun Johnson <shaun.johnson@gmail.com>
2013-05-01 08:53:45 -0400

    Added access level enum

--- src/main/java/net/lmxm/suafe/api/AccessLevel.java
+++ src/main/java/net/lmxm/suafe/api/AccessLevel.java
@@ -1,11 +1,8 @@
 package net.lmxm.suafe.api;
 
 /**
- * Created with IntelliJ IDEA.
- * User: spjohnso
- * Date: 4/29/13
- * Time: 2:57 PM
- * To change this template use File | Settings | File Templates.
+ * Access level to apply to an access rule
  */
 public enum AccessLevel {
+    READ_ONLY, READ_WRITE, DENY_ACCESS
 }
